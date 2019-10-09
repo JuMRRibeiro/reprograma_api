d91303d5809d53b2a2fb12100e8d4895780c4869
JuMRRibeiro <julianathmartins@gmail.com>
2019-10-08 21:02:39 -0300

    api

---
+++ src/controllers/alunasController.js
@@ -0,0 +1,18 @@
+const alunas = require('../model/alunas.json')
+
+exports.get = (req, res) => {
+    console.log(req.url)
+    res.status(200).send(alunas)
+}
+exports.getById = (req, res) => {
+    const id = req.params.id
+    console.log(id)
+    res.status(200).send(alunas.find(aluna => aluna.id == id))
+}
+exports.getBooks = (req, res) => {
+    const id = req.params.id
+    const aluna = alunas.find(aluna => aluna.id == id)
+    console.log(aluna)
+    res.status(200).send('bateu')
+
+}
---
+++ src/routes/alunasRoute.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const router = express.Router()
+const controller = require('../controllers/alunasController')
+
+//const alunas = require("../model/alunas.json")
+
+router.get("/", controller.get)
+
+//router.get("/", function (req, res) {
+  //  res.status(200).send(alunas)
+//})
+router.get("/: id", controller.getById)
+router.get("/:id/books", controller.getBooks)
+
+module.exports = router
